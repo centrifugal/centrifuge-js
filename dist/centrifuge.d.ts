9fa4cda8598004c051b13a479c4c876821c7ea16
Alexander Emelin <frvzmb@gmail.com>
2019-06-12 10:51:03 +0300

    embed EventEmitter definition

--- dist/centrifuge.d.ts
+++ dist/centrifuge.d.ts
@@ -3,9 +3,27 @@
 // Definitions by: Jekaspekas <https://github.com/jekaspekas>
 // TypeScript Version: 3.4.5
 
-import EventEmitter from 'events';
-
 export = Centrifuge;
+
+// From https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/node/globals.d.ts
+declare class EventEmitter {
+    addListener(event: string | symbol, listener: (...args: any[]) => void): this;
+    on(event: string | symbol, listener: (...args: any[]) => void): this;
+    once(event: string | symbol, listener: (...args: any[]) => void): this;
+    removeListener(event: string | symbol, listener: (...args: any[]) => void): this;
+    off(event: string | symbol, listener: (...args: any[]) => void): this;
+    removeAllListeners(event?: string | symbol): this;
+    setMaxListeners(n: number): this;
+    getMaxListeners(): number;
+    listeners(event: string | symbol): Function[];
+    rawListeners(event: string | symbol): Function[];
+    emit(event: string | symbol, ...args: any[]): boolean;
+    listenerCount(type: string | symbol): number;
+    // Added in Node 6...
+    prependListener(event: string | symbol, listener: (...args: any[]) => void): this;
+    prependOnceListener(event: string | symbol, listener: (...args: any[]) => void): this;
+    eventNames(): Array<string | symbol>;
+}
 
 declare class Centrifuge extends EventEmitter {
     constructor(url: string, options?: Centrifuge.Options);

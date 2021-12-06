7419a040fd05d9313830aff289808b438b4479bc
Alexander Emelin <frvzmb@gmail.com>
2021-12-06 10:23:22 +0300

    check state before setting opts

--- src/subscription.js
+++ src/subscription.js
@@ -214,14 +214,14 @@ export default class Subscription extends EventEmitter {
   };
 
   subscribe(opts) {
+    if (this._status === _STATE_SUCCESS) {
+      return;
+    }
     if (opts && opts.since) {
       this._centrifuge._setSubscribeSince(this, opts.since);
     }
     if (opts && opts.data) {
       this._setSubscribeData(opts.data);
-    }
-    if (this._status === _STATE_SUCCESS) {
-      return;
     }
     this._noResubscribe = false;
     this._centrifuge._subscribe(this);

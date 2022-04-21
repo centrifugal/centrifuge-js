3139561300d816fe6fab8587d18a45c7cf2a5569
Artur Wierzbicki <artur@arturwierzbicki.com>
2022-04-21 15:25:18 +0400

    export js enum

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 import { Centrifuge } from './centrifuge.js';
 export default Centrifuge;
+
+export const EventNames = {
+  state: 'state',
+  connecting: 'connecting',
+  connected: 'connected',
+  disconnected: 'disconnected',
+  error: 'error',
+
+  // Server-side subscription events.
+  publication: 'publication',
+  join: 'join',
+  leave: 'leave',
+  subscribed: 'subscribed',
+  subscribing: 'subscribing',
+  unsubscribed: 'unsubscribed'
+};
